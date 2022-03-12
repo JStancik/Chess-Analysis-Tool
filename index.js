05c74c70a926666f675a7b4af78bcf5f844b44f7
Jonathan Stancik <jonathan.e.stancik@gmail.com>
2022-03-11 20:59:39 -0500

    Added basic API calling ability

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+baseApiCall = "https://api.chess.com/pub/player/"
+callData = "checkmated56"
+
+
+async function getChessData(callData) {
+	let res = await fetch(baseApiCall+callData)
+	let json = await res.json()
+	console.log(json)
+	return await json
+}
+
+print(getChessData(callData))
